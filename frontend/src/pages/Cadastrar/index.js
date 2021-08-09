3d6e53f98e4b00a921ba653eaab949b895475df1
Carlos Domingues <crdpc@yahoo.com.br>
2021-08-08 22:24:45 -0300

    Adicionado formulário de cadastro

--- frontend/src/pages/Cadastrar/index.js
+++ frontend/src/pages/Cadastrar/index.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Menu } from '../../components/Menu';
 
 export const Cadastrar = () => {
+
+    const [produto, setProduto] = useState({
+        nome: '',
+        valor: '',
+        quantidade: ''    
+    });
+
+    const valueInput = e => setProduto({ ...produto, [e.target.name]: e.target.value});
+
+    const addProduto = async e => {
+        e.preventDefault();
+        console.log("Valor: " + produto.valor);
+    }
 
     return(
         <>
 
         <Menu />
         <h1>Cadastrar</h1>
+
+        <form onSubmit={addProduto}> 
+            <label>Nome: </label>
+            <input type="text" name="nome" placeholder="Nome do produto" onChange={valueInput} /> <br/><br/>
+            
+            <label>Preço: </label>
+            <input type="text" valor="valor" placeholder="Preço do produto" onChange={valueInput} /> <br/><br/>           
+
+            <label>Quantidade: </label>
+            <input type="number" name="quantidade" placeholder="Quantidade do produto" onChange={valueInput} /> <br/><br/>
+
+            <button type="submit">Cadastrar</button>
+
+        </form>
+
         </>
     );
 }
